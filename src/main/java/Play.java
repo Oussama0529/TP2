b812ba1376fd3a41e45e693162b429bd4f68b74e
ounajib <ounajib@ada.local.isima.fr>
2023-10-30 16:42:48 +0100

    static variables and type verification

--- src/main/java/Play.java
+++ src/main/java/Play.java
@@ -1,10 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Play {
+  public List<String> typelist = List.of("comedy", "tragedy", "history","pastoral");
 
   public String name;
   public String type;
 
   public Play(String name, String type) {
     this.name = name;
+    Objects.requireNonNull(typelist.contains(type),"type must not be null ");
     this.type = type;
   }
+  
 }

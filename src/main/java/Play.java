378d0b220e16a7d7aa6d694f6fc95d3af8666b7c
ounajib <ounajib@ada.local.isima.fr>
2023-10-30 17:20:00 +0100

    Adding HTMl content

--- src/main/java/Play.java
+++ src/main/java/Play.java
@@ -14,4 +14,46 @@ public class Play {
     this.type = type;
   }
   
+  public static int CalculateVolumeCreditsIncrease(int audience,String type){
+    int volumeCreditsIncrease =0 ;
+    
+    volumeCreditsIncrease += Math.max(audience - 30, 0);
+
+    if ("comedy".equals(type)) volumeCreditsIncrease += Math.floor(audience / 5);
+
+    return volumeCreditsIncrease;
+  }     
+  public static double CalculateAmount(int audience,String type){
+    double thisAmount  = 0;
+    switch (type) {
+        case "tragedy":
+          thisAmount = CalculateAmountTragedy(audience);
+          break;
+        case "comedy":
+          thisAmount = CalculateAmountComedy(audience);
+          break;
+        default:
+          throw new Error("unknown type: ${play.type}");
+      }
+    return thisAmount;
+  } 
+
+  private static double CalculateAmountTragedy(int audience) {
+    double thisAmount = 400;
+    if (audience > 30) {
+      thisAmount += 10 * (audience - 30);
+    }
+    return thisAmount;
+  }  
+
+  private static double CalculateAmountComedy(int audience) {
+    double thisAmount = 300;
+    if (audience > 20) {
+      thisAmount += 100 + 5 * (audience - 20);
+    }
+    thisAmount += 3 *audience;
+    return thisAmount;
+  }  
+
+
 }
--- src/main/java/StatementPrinter.java
+++ src/main/java/StatementPrinter.java
@@ -1,3 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.NumberFormat;
 import java.util.*;
 
@@ -14,8 +17,8 @@ public class StatementPrinter {
     for (Performance perf : invoice.performances) {
       Play play = plays.get(perf.playID);
 
-      double thisAmount =CalculateAmount(perf.audience, play.type);
-      volumeCredits += CalculateVolumeCreditsIncrease(perf.audience,play.type);
+      double thisAmount =Play.CalculateAmount(perf.audience, play.type);
+      volumeCredits += Play.CalculateVolumeCreditsIncrease(perf.audience,play.type);
 
       // print line for this order
       result.append(String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount), perf.audience));
@@ -26,45 +29,74 @@ public class StatementPrinter {
     return result;
   }
 
-  private int CalculateVolumeCreditsIncrease(int audience,String type){
-    int volumeCreditsIncrease =0 ;
-    
-    volumeCreditsIncrease += Math.max(audience - 30, 0);
+  public String toHtml(Invoice invoice, HashMap<String,Play> plays) {
 
-    if ("comedy".equals(type)) volumeCreditsIncrease += Math.floor(audience / 5);
+    StringBuilder result = new StringBuilder();
+    NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
 
-    return volumeCreditsIncrease;
-  }     
-  private double CalculateAmount(int audience,String type){
-    double thisAmount  = 0;
-    switch (type) {
-        case "tragedy":
-          thisAmount = CalculateAmountTragedy(audience);
-          break;
-        case "comedy":
-          thisAmount = CalculateAmountComedy(audience);
-          break;
-        default:
-          throw new Error("unknown type: ${play.type}");
-      }
-    return thisAmount;
-  } 
+    double totalAmount = 0;
+    int volumeCredits = 0;
 
-  private double CalculateAmountTragedy(int audience) {
-    double thisAmount = 400;
-    if (audience > 30) {
-      thisAmount += 10 * (audience - 30);
+    for (Performance perf : invoice.performances) {
+      Play play = plays.get(perf.playID);
+
+
+      double thisAmount = Play.CalculateAmount(perf.audience,play.type);
+
+      volumeCredits += Play.CalculateVolumeCreditsIncrease(perf.audience,play.type);
+
+      // append line for this order
+      result.append(
+        String.format(
+          "<tr> <td>%s</td> <td>%s</td> <td>%s </td></tr> ",
+          play.name,
+          frmt.format(thisAmount),
+          perf.audience
+        )
+      );
+      totalAmount += thisAmount;
     }
-    return thisAmount;
-  }  
+    result.append(
+      String.format("<tr><td>Total owed</td><td>%s</td></tr>", frmt.format(totalAmount))
+    );
+    result.append(String.format("<tr><td>Fidelity points earned</td><td>%s</td></tr>", volumeCredits));
+ 
 
-  private double CalculateAmountComedy(int audience) {
-    double thisAmount = 300;
-    if (audience > 20) {
-      thisAmount += 100 + 5 * (audience - 20);
-    }
-    thisAmount += 3 *audience;
-    return thisAmount;
-  }  
+    String htmlContent = 
+      "<!DOCTYPE html>\n" +
+        "<html>\n" +
+          "<head>\n" +
+            "<title>Generated HTML File</title>\n" +
+            "<style> table, th, td {border: 1px solid black;}</style>"+
+          "</head>\n" +
+            "<body>\n" +
+            "<h1>" + "Invoice" + "</h1>" +
+            "<p>" + "Client :" + invoice.customer + "</p>" +
+            "<table>"+
+              "<tr> "+ "<th>"+"Piece"+"</th>"+"<th>"+"Price"+"</th>"+"<th>"+"Seats Solde"+"</th>"+"</tr>"+ 
+              result +
+            "</table>"+
+          "</body>\n" +
+        "</html>";
+    return htmlContent;
+  }
+
+
+//  public static void main(String[] args) {
+//          String htmlFilePath = "/home/local.isima.fr/ounajib/Desktop/TP2gl/src/test/java/StatementPrinterTests.html.approved.txt"; // Path to the output HTML file
+//          HashMap<String, Play> plays = new HashMap<>();
+//         plays.put("hamlet",  new Play("Hamlet", "tragedy"));
+//         plays.put("as-like",  new Play("As You Like It", "comedy"));
+//         plays.put("othello",  new Play("Othello", "tragedy"));
+
+//         Invoice invoice = new Invoice("BigCo", List.of(
+//                 new Performance("hamlet", 55),
+//                 new Performance("as-like", 35),
+//                 new Performance("othello", 40)));
+
+//             toHtml(invoice, plays, htmlFilePath);
+//      }
+
+
 
 }
